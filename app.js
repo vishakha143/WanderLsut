d3cc8aa4d2bd675c803250a83ca37cd58bc5ef8d
vishakha <vishakha@gmail.com>
2024-08-06 12:30:27 +0530

    some changes

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(express.static(path.join(__dirname , "/public")));
 const store = MongoStore.create({
   mongoUrl :dbUrl,
   crypto : {
-    secret : process.env.SECRETE
+    secret : process.env.SECRET
   },
   touchAfter : 24*3600,
 });
@@ -63,7 +63,7 @@ store.on("error" , ()=>{
 
 const sessionOption = {
     store,
-    secret :process.env.SECRETE,
+    secret :process.env.SECRET,
     resave : false,
     saveUninitialized : true,
     cookie : {
